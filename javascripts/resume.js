e17fc129a55ff9a49e4bbdf8e50b002d870ecaad
owen0271 <owen0271@users.noreply.github.com>
2016-12-13 15:08:40 -0500

    Update resume.js

--- javascripts/resume.js
+++ javascripts/resume.js
@@ -2,7 +2,6 @@
  * Created by owen0271 on 2016/12/6.
  */
 $(document).ready(function () {
-    alert("hello world");
     $(".image_slider").bxSlider({
         mode: 'fade',
         slideWidth: 1600,
